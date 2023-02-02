5e39d1da504849ee1f2a83cc298a185a01aed93b
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-03 01:02:58 +0900

    Add files via upload
    for "get"

--- 강서진/app.py
+++ 강서진/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, g, session, request, jsonify, render_template
-# from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import sqlite3
 
 # for checklist
 from models import User, Animal, Routine, ChecklistDefault, ChecklistRoutine
-from datetime import date
+import datetime
 from connect_db import db
 
 app = Flask(__name__)
@@ -20,73 +18,71 @@ db.init_app(app)
 Migrate(app,db)
 
 
-# @bp.route('/routine', methods=['POST']) 
-# def routine():
-#     checks = request.get_json() #json받기
-    
-#     #json에서 각 값 임의 변수에 저장
-#     routine_id = checks['routine_id'] 
-#     routine_name = checks['routine_name']
-#     date = checks['date']
-#     animal_id = checks['animal_id']
-
-#     #모델로 routine 객체 만들기 
-#     routine = Routine(routine_id=routine_id, animal_id=animal_id, routine_name=routine_name, date=date) 
-
-#     #db에 저장, 업데이트
-#     db.session.add(routine)
-#     db.session.commit()
-
-#     #json 반환
-#     return jsonify(checks)
-
-
 @app.route('/', methods=["GET", "POST"])
 def checklist():
-    # 1. session['login']
-    # 2. session['curr_animal']
-    # 3. session['routine_id']
+    # 1. session['login'] 로그인한 유저
+    # 2. session['curr_animal'] 관리중인 동물
     session['login'] = 'test'
     session['curr_animal'] = 1
 
-    routines = Routine.query.filter(Routine.animal_id ==session['curr_animal'])
 
-    # checklist 입력된 값 db에 저장
-    checks = request.get_json() 
+    # checklist db에 저장
+    if request.method=="POST":
     
-    currdate = date.today()
-    animal_id = checks['animal_id']
-    food = checks['food']
-    bowels = checks['bowels']
-    note = checks['note']
-    routine_id = checks['routine_id']
-    routine_name = checks['routine_name']
-    status = checks['status']
+        checks = request.get_json() 
+        
+        currdate = datetime.datetime.now().date()
+        animal_id = checks['animal_id']
+        food = checks['food']
+        bowels = checks['bowels']
+        note = checks['note']
 
+        # multiple routines --> ?
+        routine_id = checks['routine_id']
+        routine_name = checks['routine_name']
+        status = checks['status']
 
-    checklist_default = ChecklistDefault() 
-    checklist_default.currdate = currdate
-    checklist_default.animal_id = animal_id
-    checklist_default.food = food
-    checklist_default.bowels = bowels
-    checklist_default.note = note
+        # model class에 init을 만들면 이렇게 안써도 되긴 함
+        checklist_default = ChecklistDefault() 
+        checklist_default.currdate = currdate
+        checklist_default.animal_id = animal_id
+        checklist_default.food = food
+        checklist_default.bowels = bowels
+        checklist_default.note = note
+        
+        checklist_routine = ChecklistRoutine()
+        checklist_routine.currdate = currdate
+        checklist_routine.animal_id = animal_id
+        checklist_routine.routine_id = routine_id
+        checklist_routine.routine_name = routine_name
+        checklist_routine.status = status
+
+        
+        db.session.add(checklist_default)
+        db.session.add(checklist_routine)
+
+        db.session.commit()
+        
+        return jsonify(checks)
     
-    checklist_routine = ChecklistRoutine()
-    checklist_routine.currdate = currdate
-    checklist_routine.animal_id = animal_id
-    checklist_routine.routine_id = routine_id
-    checklist_routine.routine_name = routine_name
-    checklist_routine.status = status
 
-    
-    db.session.add(checklist_default)
-    db.session.add(checklist_routine)
+    # checklist 열람
+    else:
+        routines = Routine.query.filter(Routine.animal_id == session['curr_animal']) # 1,2,3,4
 
-    db.session.commit()
-    
-    return jsonify(checks)
+        weekdays = ['mon','tue','wed','thu','fri','sat','sun']
 
+        weekly_routines = []
 
+        for weekday in weekdays: 
+            lst = []
+            for routine in routines:
+                if routine.weekday == weekday:
+                    lst.append(routine)
+
+            weekly_routines.append(lst)
+
+    return render_template('index.html', routines=routines, weekly_routines=weekly_routines)
 
 if __name__ == "__main__":
     app.run(debug=True)
