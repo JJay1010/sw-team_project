907afca87a259f2a86acab888dd10cff2bfcb9d7
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-04 15:00:25 +0900

    Add files via upload

--- 강서진/app.py
+++ 강서진/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, session, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 from flask_migrate import Migrate
 
 # for checklist
@@ -7,7 +8,7 @@ from models import User, Animal, Routine, ChecklistDefault, ChecklistRoutine
 import datetime
 from connect_db import db
 import json
-import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -29,32 +30,76 @@ def to_weekday(num):
 
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/checklist', methods=["GET", "POST"])
 def checklist():
-    # 1. session['login'] 로그인한 유저
-    # 2. session['curr_animal'] 관리중인 동물
     session['login'] = 'test'
     session['curr_animal'] = 1
 
+    if request.method=="GET": # GET
+
+        # 1. 오늘 처음 기록 입력시 -- 오늘의 routines json 반환
+        # 2. 기록 수정 + 추가 -- checklist_default, checklist_routines json 반환
+
+        checklist_default = ChecklistDefault.query.filter(and_(ChecklistDefault.animal_id == session['curr_animal'], 
+                                                                ChecklistDefault.currdate == datetime.datetime.now().date()))
+
+        checklists_routine = ChecklistRoutine.query.filter(and_(ChecklistRoutine.animal_id == session['curr_animal'], 
+                                                                ChecklistRoutine.currdate == datetime.datetime.now().date()))
+
+        routines = Routine.query.filter(and_(Routine.animal_id == session['curr_animal'], Routine.weekday == str(datetime.datetime.now().weekday())))
+
+        # routine이 없을 시, default 폼만 반환
+        if routines == None:
+            return render_template('index.html')
+
+
+        # routine이 있을 시, default 폼에 routine json 반환
+
+        else: 
+
+            # 오늘 요일과 일치하는 routine 딕셔너리를 리스트에 넣어 리턴
+            today_routines = []
+            for routine in routines:
+                routine = routine.__dict__
+                del routine['_sa_instance_state']
+                today_routines.append(routine)
+
+
+            # 오늘 날짜와 일치하는 checklist_default 1개 레코드 반환 
+            for checklist_d in checklist_default:
+                checklist_d = checklist_d.__dict__
+                del checklist_d['_sa_instance_state']
+
+
+            # 오늘 날짜와 일치하는 checklists_routine n개 리스트에 넣어 반환
+            today_checklist_r = []
+            for checklist_r in checklists_routine:
+                checklist_r = checklist_r.__dict__
+                del checklist_r['_sa_instance_state']
+                today_checklist_r.append(checklist_r)
+
+
+            print(today_checklist_r)
+
+            jchecklist = jsonify(checklist_d)
+            print(type(jchecklist)) # <class 'flask.wrappers.Response'>
+
+
+            return render_template('index.html', routines=jsonify(today_routines), checklist=jsonify(checklist_d), checklist_r=jsonify(today_checklist_r))
+   
 
     # checklist db에 저장
-    # 0. 오늘 처음 기록 -- checklist_default, checklist_routines 오늘 날짜 비어있으면 전부 add
-    # 1. 기록 수정 및 추가 -- 오늘 날짜 비어있는 부분에만 add, 나머지는 update?
-    if request.method=="POST":
+    # 기록 저장
+    else:   # POST
     
         checks = request.get_json() 
-        
+
+        # default box
         currdate = datetime.datetime.now().date()
         animal_id = checks['animal_id']
         food = checks['food']
         bowels = checks['bowels']
         note = checks['note']
-
-        # uhh multiple routines --> will probably numbered?
-
-        routine_id = checks['routine_id']
-        routine_name = checks['routine_name']
-        status = checks['status']
 
         checklist_default = ChecklistDefault() 
         checklist_default.currdate = currdate
@@ -62,6 +107,16 @@ def checklist():
         checklist_default.food = food
         checklist_default.bowels = bowels
         checklist_default.note = note
+
+
+        # routine box
+        # ------------------
+        # 루틴이 1개 이상일 때는? 
+        # ------------------
+        routine_id = checks['routine_id']
+        routine_name = checks['routine_name']
+        status = checks['status']
+
         
         checklist_routine = ChecklistRoutine()
         checklist_routine.currdate = currdate
@@ -76,53 +131,9 @@ def checklist():
 
         db.session.commit()
         
-        return jsonify(checks)
+        return render_template('checkform.html', checks = jsonify(checks))
     
 
-    else: # GET
-
-        # 0. 설정한 routine이 없을 때 -- default만 표시
-        # 0-0. 완전 처음 사용해서 routine도, default 기록도 없을 때
-        # 1. 오늘 처음 기록 입력시 -- 오늘의 routines json 반환
-        # 2. 기록 수정 + 추가 -- checklist_default, checklist_routines json 반환
-
-        checklists_default = ChecklistDefault.query.filter(ChecklistDefault.animal_id == session['curr_animal'])
-        checklists_routine = ChecklistRoutine.query.filter(ChecklistRoutine.animal_id == session['curr_animal'])
-        routines = Routine.query.filter(Routine.animal_id == session['curr_animal'])
-
-        if routines == None:
-            print("no routines")
-
-        if checklists_default == None:
-            print("no default checklist")
-
-        for checklist_default in checklists_default:
-            if checklist_default.currdate == str(datetime.datetime.now().date()):
-                checklist_default = checklist_default.__dict__
-                del checklist_default['_sa_instance_state']
-
-
-
-
-
-        # 1. 
-        today_weekday = str(datetime.datetime.now().weekday()) # 4
-
-        
-
-        today_lst = []
-        for routine in routines:
-            if routine.weekday == today_weekday:
-                routine = routine.__dict__
-                del routine['_sa_instance_state']
-                today_lst.append(routine)
-
-        # for lst in today_lst:
-            # lst 형태는 {'animal_id': 1, 'routine_name': 'teeth', 'routine_id': 4, 'weekday': '4'}, dict
-             # 4 = fri
-
-
-    return         
 
 if __name__ == "__main__":
     app.run(debug=True)
