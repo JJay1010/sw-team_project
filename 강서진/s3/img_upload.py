e338a022e2d5c92eed148ba32c5d20f6ca211989
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-07 12:05:25 +0900

    Add files via upload

---
+++ 강서진/s3/img_upload.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import datetime
+# from predict import padding, mk_img, predict_result
+
+from config import AWS_S3_BUCKET_NAME, AWS_S3_BUCKET_REGION
+from connect_s3 import s3_connection, s3_put_object, s3_get_object
+
+import os
+
+app = Flask(__name__)
+
+
+s3 = s3_connection()
+
+@app.route('/', methods = ['GET', 'POST'])
+def file_upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + ".png"
+        f.filename = newname
+        imgpath = f"./static/{f.filename}"
+        f.save(imgpath)
+
+        ret = s3_put_object(s3, AWS_S3_BUCKET_NAME, imgpath, f.filename)
+
+        if ret:
+            print("success")
+
+            img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{f.filename}"
+
+            return render_template('index.html', img_url=img_url)
+
+        else:
+            print("failed")
+        return render_template('index.html')
+
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
