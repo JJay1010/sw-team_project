29e4622887efdc19348663a2113abd68e300c803
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-08 22:49:48 +0900

    added journal_update

--- 강서진/s3/journal.py
+++ 강서진/s3/journal.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, session, Blueprint
+from flask import url_for, redirect, jsonify, request, session, Blueprint
 import boto3
 import datetime
 from connect_db import db
@@ -33,20 +33,6 @@ def s3_connection():
         print('ERROR_S3_CONNECTION_FAILED') 
 
 
-# 파일 업로드 받아서 s3 업로드, 이미지 링크 반환
-# def put_s3_img_url():
-#     f = request.files['file']
-#     newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + ".png"
-
-#     imgpath = f"./static/{newname}"
-#     f.save(imgpath)
-
-#     s3.upload_file(imgpath, AWS_S3_BUCKET_NAME, newname)
-#     img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
-    
-#     return img_url
-
-
 s3 = s3_connection()
 
 
@@ -65,14 +51,13 @@ def journal():
     if request.method == 'GET':
 
         today_entry = Journal.query.filter(and_(Journal.user_id==current_user,
+                                                Journal.animal_id==current_animal,
                                                 Journal.currdate == current_date)).first()
         
         # 오늘의 기록 존재시
         if today_entry != None:
-            today_entry = today_entry.__dict__
-            del today_entry['_sa_instance_state']
 
-            return jsonify(today_entry)
+            return redirect(url_for('journal.journal_update'))
 
         # 오늘의 기록이 없을 시
         else:
@@ -113,9 +98,72 @@ def journal():
 
         db.session.commit()
     
-        return jsonify(new_entry)
+        return "success"
+
+
+@bp.route('/journal_update', methods=["GET","POST"])
+def journal_update():
+    # 나중에 삭제
+    session['login'] = 'test'
+    session['curr_animal'] = 1
+
+    current_user = session['login']
+    current_animal = session['curr_animal']
+    current_date = datetime.datetime.now().date()
+
+    if request.method == 'GET':
+
+        today_entry = Journal.query.filter(and_(Journal.user_id==current_user,
+                                                Journal.animal_id==current_animal,
+                                                Journal.currdate == current_date)).first()
+
+        today_entry = today_entry.__dict__
+        del today_entry['_sa_instance_state']
+        return jsonify(today_entry)
+
+    else: #POST
+
+        today_entry = Journal.query.filter(and_(Journal.user_id==current_user,
+                                                Journal.animal_id==current_animal,
+                                                Journal.currdate == current_date)).first()
+
+        journal_entry = request.form
+        journal_entry = json.loads(journal_entry['data'])
+
+        title = journal_entry['title']
+        content = journal_entry['content']
+        currdate = current_date
+
+        f = request.files['file']
+        # 새로 이미지 업로드 시
+        if f:
+            # 기존의 이미지 삭제
+            s3.delete_object(
+                Bucket = AWS_S3_BUCKET_NAME,
+                Key = (today_entry.image).split('/')[-1]
+            )
+
+            newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + ".png"
+
+            imgpath = f"./static/{secure_filename(newname)}"
+            f.save(imgpath)
+
+            s3.upload_file(imgpath, AWS_S3_BUCKET_NAME, newname)
+            img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
+
+            today_entry.title = title
+            today_entry.content = content
+            today_entry.image = img_url
+
+        # 새로 이미지 업로드 X --> 기존의 image 칼럼 데이터 그대로 유지
+        else:
+
+            today_entry.title = title
+            today_entry.content = content
+
+        db.session.commit()
+    return "update successful"
 
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
