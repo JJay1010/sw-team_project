bdd57d448b922a7a3bb4bf3c35a2af3ab5563f4c
bokyung <77317533+SeongBoKyung@users.noreply.github.com>
2023-02-09 16:46:20 +0900

    FEAT: 동물 등록 기능 추가

--- 성보경/app.py
+++ 성보경/app.py
@@ -2,18 +2,21 @@ from flask import Flask, session, request, jsonify, render_template, redirect
 from flask import flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
 from markupsafe import escape
 from flask_bcrypt import Bcrypt
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 import os
-from models import User
+from models import User, Animal
 from connect_db import db
 from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
+
+WTF_CSRF_ENABLED = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pet_test.db'
 app.config['SECRET_KEY'] = "test"
@@ -23,8 +26,8 @@ db.init_app(app)
 Migrate(app,db)
 bcrypt = Bcrypt(app)
 
-@app.route('/register', methods=['GET','POST']) #GET(정보보기), POST(정보수정) 메서드 허용
-def register():
+@app.route('/register', methods=['GET','POST'])
+def register_User():
     forms = RegisterForm()
     if forms.validate_on_submit():
         user = User()
@@ -55,6 +58,7 @@ def login():
     forms = LoginForm() #로그인 폼 생성
     if forms.validate_on_submit(): #유효성 검사
         session['userid'] = forms.data.get('userid') #form에서 가져온 userid를 session에 저장
+        flash("환영합니다.")
     
         return redirect('/') #로그인에 성공하면 홈화면으로 redirect
             
@@ -72,6 +76,31 @@ def main():
             "<br><a href = '/logout'>로그아웃</a>"
 
     return "로그인 해주세요. <br><a href = '/login'> 로그인 하러가기! </a>" # 로그인이 안될 경우
+
+@app.route('/registerAnimal', methods=['GET','POST'])
+def register_animal():
+    param = request.get_json()
+
+    if 'userid' not in session:
+        return redirect('/')
+    else:
+        #userid = session['userid'] #세션에 저장된 id값 받아오기
+
+        user = User.query.filter_by(userid='test').first() #속성 userid가 userid와 동일한 객체 가져오기
+
+        animal = Animal(user = user) #relationship에 user 객체 넘겨주기
+
+        animal.animal_name = param['animal_name']
+        animal.bday = param['bday']
+        animal.sex = param['sex']
+        animal.neutered = param['neutered']
+        animal.weight = param['weight']
+
+        db.session.add(animal)
+        db.session.commit()
+        flash("등록이 완료되었습니다.")
+
+    return jsonify(param) 
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 성보경/models.py
+++ 성보경/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-#db = SQLAlchemy()           #SQLAlchemy를 사용해 데이터베이스 저장
 from connect_db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -9,16 +8,21 @@ class User(db.Model):
     userid = db.Column(db.String(32), unique=True, nullable=False, primary_key=True)
     password = db.Column(db.String(8), nullable=False)
 
-
-    # def __init__(self, userid, email, password, **kwargs):
-    #   self.userid = userid
-    #   self.email = email
- 
-    #   self.set_password(password)
-
-
     def set_password(self, password):
         self.password = generate_password_hash(password)
  
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Animal(db.Model):
+    __tablename__='animal'
+    animal_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(), db.ForeignKey('user.userid', ondelete='CASCADE')) #사용자 삭제 시 동물도 삭제
+    user = db.relationship('User', backref=db.backref('animal_set')) #User의 객체에서 등록된 동물들 참조 가능
+    #routines = db.relationship('Routine')
+    
+    animal_name = db.Column(db.String(20), nullable=False)
+    bday = db.Column(db.String(6), nullable=True)
+    sex = db.Column(db.String(), nullable=True)
+    neutered = db.Column(db.String(), nullable=True)
+    weight = db.Column(db.Float, nullable=True)
